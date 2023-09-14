47c0c18f637090a3d9dae6cbf7f9ffa453333e33
SMuthiah5547 <72676014+SMuthiah5547@users.noreply.github.com>
2023-09-13 20:14:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 st.dataframe(fruits_to_show)
 st.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)

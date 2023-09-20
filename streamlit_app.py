534ff491f0f2a211490fac1a7187c0d84318399e
SMuthiah5547 <72676014+SMuthiah5547@users.noreply.github.com>
2023-09-20 11:18:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,21 +22,28 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select the fruit to get information.")
+  else:
+    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
+except URLError as e:
+  st.error()
 
 
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# st.write('The user entered ', fruit_choice)
+# fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # st.text(fruityvice_response.json())
-
 # write your own comment - what does the next line do? - json_normalize() converts the nested dictionaries into separate columns for each key. 
 # By default, the nested parts have column names in the format <parent key>.
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-
+# fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - Output the json output as table
-st.dataframe(fruityvice_normalized)
-st.stop()
+# st.dataframe(fruityvice_normalized)
 
+st.stop()
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
